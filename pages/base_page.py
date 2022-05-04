c59f4a936eba1874c84526f1315ae68c0a7df1d4
yankovskaya <yankovskaya.my@gmail.com>
2022-05-04 16:22:42 +0300

    feat: added is_element_present function

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
-        
+
+    def is_element_present(self, how, what):
+        try: 
+            self.browser.find_element(By.CSS_SELECTOR,"#login_link")
+        except(NoSuchElementException):
+            return False
+        return True
+
