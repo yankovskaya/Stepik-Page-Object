6d4251e9a3e4fc9c0d40ed48b466bd6327c8ee4c
yankovskaya <yankovskaya.my@gmail.com>
2022-05-04 21:37:28 +0300

    feat: add product_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,6 @@
 from pages.base_page import BasePage
 from pages.locators import MainPageLocators
 
-
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
