1ae75ed0f51b58bb74b2a37815b49a158be2e742
yankovskaya <yankovskaya.my@gmail.com>
2022-05-04 21:30:57 +0300

    feat: add alert

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,13 @@
 from pages.base_page import BasePage
-from selenium.webdriver.common.by import By
 from pages.locators import MainPageLocators
-from pages.login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click() 
+        login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept() 
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
