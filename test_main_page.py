4d2be664b2caccc73067a9c996d172709858b9d6
yankovskaya <yankovskaya.my@gmail.com>
2022-05-04 21:34:27 +0300

    refactor: delete strings

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -13,5 +14,3 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
