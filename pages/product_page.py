c91d2474794f932075d4e706a66d8bbca8bdc06d
yankovskaya <yankovskaya.my@gmail.com>
2022-05-04 21:41:25 +0300

    feat: add product_page.py

---
+++ pages/product_page.py
@@ -0,0 +1,10 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+class ProductPage(BasePage):
+        def add_product_to_basket(self):
+            self.browser.find_element(*ProductPageLocators.ADD_TO_CARD).click()
+
+        def check_product_name_and_price(self):
+            print("'{}' added to cart!".format(self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text))
+            assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text in self.browser.find_element(*ProductPageLocators.PRICE_IN_CART).text
