e2e2e9c3fe1c7244f8d4f8db68ddab175c689c6c
yankovskaya <yankovskaya.my@gmail.com>
2022-05-04 21:35:47 +0300

    feat: create ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form.well")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form.well")
+
+class ProductPageLocators():
+    ADD_TO_CARD = (By.CSS_SELECTOR,"button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main>h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".price_color")
