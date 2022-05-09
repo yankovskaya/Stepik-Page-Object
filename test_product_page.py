7077a34205cddec5ce2850b5fa97cb35a1cebc63
yankovskaya <yankovskaya.my@gmail.com>
2022-05-09 14:40:57 +0300

    feat: add new tests to test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -2,18 +2,7 @@ import time
 import pytest
 from pages.product_page import ProductPage
 
-   
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
-                            ])
+@pytest.mark.skip
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
@@ -23,4 +12,29 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.check_product_name_and_price(link)
     time.sleep(3)   
 
-    
+@pytest.mark.skip   
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(1)
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+    page = ProductPage(browser, link)
+    page.open()
+    time.sleep(1)
+    page.add_product_to_basket()
+    page.should_not_be_success_message_is_disappeared()
