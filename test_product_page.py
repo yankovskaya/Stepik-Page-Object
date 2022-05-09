d373febb3fdb98d3a901a54883704c1a1aae0f7e
yankovskaya <yankovskaya.my@gmail.com>
2022-05-09 15:15:53 +0300

    feat: add new test for login link

--- test_product_page.py
+++ test_product_page.py
@@ -22,6 +22,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.should_not_be_success_message()
 
+@pytest.mark.skip   
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
@@ -38,3 +39,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     time.sleep(1)
     page.add_product_to_basket()
     page.should_not_be_success_message_is_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
