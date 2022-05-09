defd9bc8ab4b381342975e8fe769d6080f6409c1
yankovskaya <yankovskaya.my@gmail.com>
2022-05-09 15:14:58 +0300

    refactor:move validation from MainPage to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from pages.locators import BasePageLocators
 
 
 class BasePage():
@@ -12,6 +13,13 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
